0036ddecf48527bb26fc10e9e2b6793afc0b2996
jujube6363 <113395821+jujube6363@users.noreply.github.com>
2024-03-18 17:45:01 -0500

    Update ManipulatingStrings.c

--- ManipulatingStrings.c
+++ ManipulatingStrings.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 int main(void){
     //this prints the proper nouns in the sentence
-    /*const char* arr = "HelLo iM Jack and You ";
+    const char* arr = "HelLo iM Jack and You ";
     char wordarr[10];
     int i;
     int j=0;
@@ -20,31 +20,4 @@ int main(void){
             printf("\n");
         }
         j++;
-    }*/
-    /*char* str = "abcde";
-    printf("before: %s\n", str);
-    str[1] = 'g';
-    printf("after: %c\n", str[1]);*/
-    int i=0;
-    int j=0;
-    int counter=0;
-    int letters;
-    const char* str1 = "hello";
-    const char* str2 = "hello";
-    for(int k=0; str1[k]!='\0'; k++){
-        letters++;
     }
-    while(str1[i]!='\0'&&str2[j]!='\0'){
-        if(str1[i]==str2[j]){
-            counter++;
-            i++;
-        }
-        j++;
-    }
-    if(counter==letters){
-        printf("1"); //prints 1 if the character string IS a subsequence of another string
-    }
-    else{
-        printf("0"); //prints 0 if the character string IS a subsequence of another string
-    }
-}
